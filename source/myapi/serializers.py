652d479eda40da5cd5130d7bb77de367055d0637
Evgenii <lipatowevgeniy@gmail.com>
2019-12-14 19:39:18 +0600

    The 5th stage. Add permissions

--- source/myapi/serializers.py
+++ source/myapi/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 from webapp.models import Like, Comment
 
 
@@ -8,11 +9,14 @@ class LikeSerializer(serializers.ModelSerializer):
         fields = ('id', 'photo', 'user')
 
 
+
 class CommentSerializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = Comment
         fields = ('id', 'text', 'created_at', 'author','photo')
+
+
 
 
